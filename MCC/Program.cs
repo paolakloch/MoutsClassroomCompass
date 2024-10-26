f7c416452d63f60a0069004243756eb1a290cf85
paola kloch <paola.kloch@gmail.com>
2024-10-26 18:40:45 -0300

    feat: added authentication using jwt and identity

--- MCC/Program.cs
+++ MCC/Program.cs
@@ -1,3 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+using MCC.Data;
+using MCC.Models;
+using Microsoft.AspNetCore.Identity;
+using MCC.Services;
 
 namespace MCC
 {
@@ -9,10 +14,33 @@ namespace MCC
 
             // Add services to the container.
 
+            var connString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+            builder.Services.AddDbContext<UserDBContext>(opts =>
+            {
+                opts.UseNpgsql(connString);
+            });
+
+            builder.Services
+                .AddIdentity<User, IdentityRole>()
+                .AddEntityFrameworkStores<UserDBContext>()
+                .AddDefaultTokenProviders();
+
+            builder.Services.AddAutoMapper
+                  (AppDomain.CurrentDomain.GetAssemblies());
+
+
+            builder.Services
+                .AddScoped<RegisterService>() // optei por usar scoped - o servico vai sempre ser instanciado quando houver uma requisição nova que demande uma instancia desse register service // o singleton seria 1 unico cadastro service pra todas as reqs seria a mesma instancia // add transient faz sempre uma instancia nova mesmo que na mesma req
+                .AddScoped<LoginService>()
+                .AddScoped<TokenService>();
+
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+
 
             var app = builder.Build();
 
@@ -26,7 +54,6 @@ namespace MCC
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
-
 
             app.MapControllers();
 
---
+++ MCC/Services/LoginService.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using MCC.Data.Dtos;
+using MCC.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Identity;
+
+namespace MCC.Services
+{
+    public class LoginService
+    {
+        private IMapper _mapper;
+        private UserManager<User> _userManager;
+        private SignInManager<User> _signInManager;
+        private TokenService _tokenService;
+
+        public LoginService(
+            IMapper mapper,
+            UserManager<User> userManager,
+            TokenService tokenService,
+            SignInManager<User> signInManager
+            )
+        {
+            _mapper = mapper;
+            _tokenService = tokenService;
+            _userManager = userManager;
+            _signInManager = signInManager;
+
+        }
+
+        public async Task<string> LoginAsync(LoginUserDto dto)
+        {
+            var result =
+            await _signInManager.PasswordSignInAsync(dto.UserName, dto.Password, false, false);
+
+            if (!result.Succeeded)
+            {
+                throw new Exception("Usuário não autenticado");
+            }
+
+            var user = _signInManager
+                .UserManager
+                .Users
+                .FirstOrDefault(user =>
+                user.NormalizedUserName == dto.UserName.ToUpper());
+
+            var token = _tokenService.GenerateToken(user);
+
+            return token;
+
+        }
+    }
+}
